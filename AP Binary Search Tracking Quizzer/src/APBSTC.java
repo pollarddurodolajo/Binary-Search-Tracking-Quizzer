8a16252b3846c1c76eb1c1a9506adb9f02e38c0f
jonothon.pollard <jonothon.pollard@MU-C3-19.mullen.pvt.k12.co.us>
2014-12-16 08:51:21 -0700

    init

---
+++ AP Binary Search Tracking Quizzer/src/APBSTC.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class APBSTC
+	{
+
+	public static void main(String[] args)
+		{
+		int addingNums = 0;
+		String name;
+		ArrayList listOfNums = new ArrayList();
+	
+		Scanner userInput1 = new Scanner(System.in);
+		System.out.println("Welcome user. What is your name?");
+		name = userInput1.nextLine();
+		
+		do			
+			{
+			System.out.println(name + ", imput your numbers one at a time and press enter after each imput. \nAdd numbers between one and twenty. If you've finished adding \nnumbers to the ArrayList imput '-1'");
+			Scanner userInput2 = new Scanner(System.in);
+			addingNums = userInput2.nextInt();
+			if(addingNums >= 1 && addingNums <= 20 && userInput2.hasNextInt())
+				{
+				listOfNums.add(addingNums);
+				}
+				
+			else if(addingNums <= 0 && addingNums != -1)
+				{
+				System.out.println("Please only imput numbers in the range of 1 - 20 inclusive.");
+				}
+			}	
+		while(addingNums != -1);
+
+		Collections.sort(listOfNums);
+		
+			int target = (int)(Math.random())
+			int left = 0;
+			int right = listOfNums.size() - 1;
+			
+			while (left <= right)
+				{
+				int middle = (left + right) / 2;
+				if (target < listOfNums.get(middle))
+				{
+				right = middle - 1;
+				}
+				else if (target > elements[middle])
+					{
+					left = middle + 1;
+					}
+				else
+					{
+					System.out.println(middle);
+					}
+				}
+			System.out.println(-1);
+			
+		}
+	}
